d0a7918559b50586f03c6d73f9b0c58052bfa707
smrkarim555 <smrkarim555@gmail.com>
2026-09-07 03:11:49 +0600

    Sync CURRENT_COMMIT to 2468e05

--- auth-config.js
+++ auth-config.js
@@ -8,7 +8,7 @@ const APP_CONFIG = {
 
   // 2. GitHub Repository (username/repo for auto-updates)
   GITHUB_REPO: "smrkarim555/pngtree-ai-metadata",
-  CURRENT_COMMIT: "9342cea",
+  CURRENT_COMMIT: "2468e05",
 
   // 3. Google OAuth Client ID (for Google Accounts sign-in)
   GOOGLE_CLIENT_ID: "916776251769-9cale3jpkbj83m4s0q55itkrmhj70sn2.apps.googleusercontent.com",
